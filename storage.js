d560b8e11a6397aba5c60bde075ca614eb62a6b2
naveenm <naveen.malhotra@intelligentpathways.com.au>
2017-01-05 14:32:22 +1000

    storage example

---
+++ storage.js
@@ -0,0 +1,17 @@
+// Try me locally using: wt serve --storage-file counter.json counter.js
+
+module.exports = function(context, cb) {
+    context.storage.get(function(error, data) {
+        if (error) return cb(error);
+        if (!data) data = {};
+        if (!data.counter) data.counter = 0;
+
+        data.counter++;
+
+        context.storage.set(data, function(error) {
+            if (error) return cb(error);
+
+            cb(null, data);
+        });
+    });
+}
