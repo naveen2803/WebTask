becf3f159fbad08d3294d13b649f3afe25ff4d84
naveenm <naveen.malhotra@intelligentpathways.com.au>
2017-01-05 14:17:32 +1000

    updated file

--- queryStringSample.js
+++ queryStringSample.js
@@ -1,5 +1,6 @@
 /*
  *  This prints the querystring parameters passed in the url(name)
+ *  https://wt-naveen-malhotra28-gmail-com-0.run.webtask.io/queryStringSample?name=Hello World
  */
 module.exports = function(context, cb) {
     cb(null, context.data.name);
