f52ae969af46cf00cc6b6d872b116c51ce3dc28b
naveenm <naveen.malhotra@intelligentpathways.com.au>
2017-01-05 14:08:28 +1000

    updates

--- mysqlConnect.js
+++ mysqlConnect.js
@@ -4,14 +4,14 @@ module.exports = function(context, cb) {
 
     function connect() {
         var connection = mysql.createConnection({
-            host: '50.62.176.15',
-            user: 'pavamanauser',
-            password: 'Password@12345',
-            database: 'pavamana'
+            host: 'hostname',
+            user: 'username',
+            password: 'password',
+            database: 'db_name'
         });
 
         connection.connect();
-        var query = "SELECT * FROM " + context.data.table; //aradhane, events, festivals
+        var query = "SQL Query to execute";
         connection.query(query,
             function(err, rows, fields) {
                 if (err) {
