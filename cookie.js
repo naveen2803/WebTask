6ce51554d3ecc06b7fd1c68f5cd8d7cdb01ccefd
naveenm <naveen.malhotra@intelligentpathways.com.au>
2017-01-09 11:16:52 +1000

    updated

--- cookie.js
+++ cookie.js
@@ -3,12 +3,19 @@ var wt = require('webtask-tools');
 var cookieParser = require('cookie-parser')
 
 var app = new express();
-app.use(cookieParser());
+app.use(cookieParser("This is my passphrase"));
 
 app.get('/', function(req, res) {
-    req.cookies = req.cookies ||
-        'Something...';
-    res.send(req.cookies);
+    if (req.cookies.beenHereBefore === 'yes') {
+        res.send("You have been here before");
+    } else {
+        res.cookie('beenHereBefore', 'yes');
+        res.send('This is you first time');
+    }
 });
 
+app.get('/clear', function(req, res) {
+    res.clearCookie('beenHereBefore');
+    res.redirect('/');
+});
 module.exports = wt.fromExpress(app);
